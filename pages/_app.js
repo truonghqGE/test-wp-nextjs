a66fd34fd4c1457fb983d1a10dce8bb6a9cd18ab
Nhat Nguyen <newbie.dev.js@gmail.com>
2022-04-03 13:01:32 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/globals.css";
 import React from "react";
 import "katex/dist/katex.min.css";
@@ -9,25 +8,6 @@ import { useRouter } from "next/router";
 import Category from "./category";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-=======
-import '../styles/globals.css';
-import React from 'react';
-import axiosClient, { constructCallBack } from '@/api/base/axios-client';
-import 'katex/dist/katex.min.css';
-import Category from "./category"
-import HeadSEO from "@/components/SEO/head";
-import Header from "@/components/header";
-import { SWRConfig } from "swr";
-import { useRouter } from "next/router";
-import Footer from "@/components/footer";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
->>>>>>> 49ebe2df5fb296848b7e9e9b209b99a2ae254126
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -44,38 +24,15 @@ function MyApp({ Component, pageProps }) {
   });
   return (
     <React.Fragment>
-<<<<<<< HEAD
       <Head />
-=======
->>>>>>> 49ebe2df5fb296848b7e9e9b209b99a2ae254126
       <ApolloProvider client={client}>
         <HeadSEO />
-        <Category></Category>
-<<<<<<< HEAD
-
-=======
-      <SWRConfig
-        value={{
-          fetcher: (url) => {
-            return axiosClient.get(url);
-          },
-          dedupingInterval: 60 * 60 * 1000, // 1hr
-          revalidateOnFocus: false,
-          shouldRetryOnError: false,
-        }}
-      >
->>>>>>> 49ebe2df5fb296848b7e9e9b209b99a2ae254126
+        <Category />
         <div className="archive category category-nutrition category-19 logged-in admin-bar  customize-support">
           <Header />
           <Component {...pageProps} key={asPath} />
         </div>
-
         <Footer />
-<<<<<<< HEAD
-        {/* </SWRConfig> */}
-=======
-      </SWRConfig>
->>>>>>> 49ebe2df5fb296848b7e9e9b209b99a2ae254126
       </ApolloProvider>
     </React.Fragment>
   );
